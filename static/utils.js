19775dd6a13e8e5ae6083ef8699a2866642bd73e
caionikolas <Caio.kanu@hotmail.com>
2023-06-06 22:34:16 -0300

    precoMedio

--- static/utils.js
+++ static/utils.js
@@ -61,3 +61,31 @@ function limparFormulario() {
     funcao.value = ''
     tx_corretagem.value = ''
 }
+
+const ajustarPrecoMedio = (investiments) => {
+    let soma_total = 0
+    let ultimo_pm = 0
+
+    return investiments.map((i) => {
+        i.valor_op = i.quantidade * i.valor_unit;
+        
+        i.imposto = i.valor_op * 0.0003;
+
+        if (i.tipo == 'C') {
+            i.valor_final = i.valor_op + i.imposto + i.taxa_corretagem;
+            
+            soma_total += i.quantidade
+            i.preco_medio = (((soma_total - i.quantidade) * ultimo_pm) + i.valor_final) / soma_total
+        } else {
+            i.valor_final = i.valor_op - i.imposto - i.taxa_corretagem;
+
+            soma_total -= i.quantidade
+            i.preco_medio = ultimo_pm
+            i.lucro_prejuizo = Math.round(i.valor_final - (i.preco_medio * i.quantidade), 2)
+        }
+
+        ultimo_pm = i.preco_medio
+
+        return i
+    })
+}
